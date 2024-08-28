677c8c5e86089443f5262c47acc438fffee4feaf
maria.paulina.r.v <maria.paulina.r.v@gmail.com>
2024-08-28 07:05:55 +0000

    Configurations nix

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.php83
+  ];
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql;
+  };
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
